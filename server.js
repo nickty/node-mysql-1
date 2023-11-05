a6d26531376f614d2f61bae3ca2e02fe7cf3595e
Mizanur Rahman <mizan@arogga.net>
2023-11-05 15:39:18 +0600

    frontend working

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+
+const corsOptions = {
+  exposedHeaders: 'X-Total-Count',
+};
+
+// Enable CORS for all routes
+app.use(cors(corsOptions));
 
 const dbConfig = {
   host: 'localhost',
@@ -11,11 +19,18 @@ const dbConfig = {
   database: 'myreactadmindb', // Replace with your database name
 };
 
-app.get('/', async (req, res) => {
+app.get('/users', async (req, res) => {
   try {
     const connection = await mysql.createConnection(dbConfig);
     const [rows, fields] = await connection.execute('SELECT * FROM users'); // Replace with your table name
     connection.end();
+    
+    // Calculate the total number of records
+    const totalCount = rows.length;
+
+    // Set the "X-Total-Count" header in the response
+    res.header('X-Total-Count', totalCount);
+    
     res.json(rows);
   } catch (error) {
     console.error('Error:', error);
