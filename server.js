efad579db7e5deab486e19d4045b3ef414ceb4fa
Mizanur Rahman <mizan@arogga.net>
2023-11-05 16:51:10 +0600

    get single user

--- server.js
+++ server.js
@@ -30,8 +30,38 @@ app.get('/users', async (req, res) => {
 
     // Set the "X-Total-Count" header in the response
     res.header('X-Total-Count', totalCount);
-    
+
     res.json(rows);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Create a route to get a single user by ID
+app.get('/users/:id', async (req, res) => {
+  try {
+    const userId = req.params.id; // Get the user ID from the URL parameter
+
+    // Establish a connection to the MySQL database
+   const connection = await mysql.createConnection(dbConfig);
+
+    // Execute a SQL query to retrieve the user with the specified ID
+    const [rows] = await connection.execute('SELECT * FROM users WHERE id = ?', [userId]);
+    connection.end();
+
+    if (rows.length === 0) {
+      // If no user with the specified ID is found, return a 404 response
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      // If a user is found, return the user data
+      // Calculate the total number of records
+    const totalCount = rows.length;
+
+    // Set the "X-Total-Count" header in the response
+    res.header('X-Total-Count', totalCount);
+      res.json({ user: rows[0] });
+    }
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
